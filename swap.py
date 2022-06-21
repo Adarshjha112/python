0a6360aec44451d6150c37678a1da7662cdcff45
Adarsh Jha <56366603+Adarshjha112@users.noreply.github.com>
2022-06-21 12:10:02 +0530

    swap in python

---
+++ swap.py
@@ -0,0 +1,14 @@
+#swap two numbers
+
+
+a=(int(input("Enter a value")))
+b=(int(input("enter b value")))
+
+print("A and B values before swapping:")
+print("a = ",a)
+print("b = ",b)
+
+a,b = b,a
+print("A and B values after swapping:")
+print("a = ",a)
+print("b = ",b)
